e7ed7870517552b161abef477a0715202a92768a
eyllanesc <e.yllanescucho@gmail.com>
2016-10-24 01:14:37 -0500

    eliminar redundancia

--- Atoms/Atoms.py
+++ Atoms/Atoms.py
@@ -6,9 +6,7 @@ Module implementing Atoms.
 from math import pi, cos, sin
 
 from PyQt5.QtCore import (QParallelAnimationGroup, QPointF, QPropertyAnimation, QRectF, Qt, qrand, qsrand, QTime)
-from PyQt5.QtGui import (QBrush, QColor, QPen)
-from PyQt5.QtGui import QPainterPath
-from PyQt5.QtGui import QTransform
+from PyQt5.QtGui import (QBrush, QColor, QPen, QPainterPath, QTransform)
 from PyQt5.QtWidgets import (QGraphicsObject, QGraphicsScene, QWidget)
 
 from Ui_Atoms import Ui_Atoms
@@ -30,7 +28,7 @@ class Atoms(QWidget, Ui_Atoms):
         self.setupUi(self)
         scene = QGraphicsScene()
         scene.setSceneRect(0, 0, 200, 200)
-        n = 12
+        n = 4
         group = QParallelAnimationGroup(self)
         now = QTime.currentTime()
         qsrand(now.msec())
@@ -40,30 +38,32 @@ class Atoms(QWidget, Ui_Atoms):
         transform = QTransform()
         transform.translate(center.x(), center.y())
 
+        alpha = 180 / n
+
+        p = 30
+        path = QPainterPath()
+        path.addEllipse(QPointF(0, 0), a, b)
+
         for j in range(n):
-            color = QColor(qrand() % 254, qrand() % 254, qrand() % 254)
-            item = CircleObject(color=color)
-            animation = QPropertyAnimation(item, b'pos')
-            l = []
-            initial = (qrand() % 360)*pi/180
-            alpha = 180 * j / n
+            color = QColor(qrand() % 255, qrand() % 255, qrand() % 255)
 
             transform.rotate(alpha)
-            alpha *= pi/180
-            path = QPainterPath()
-            path.addEllipse(QPointF(0, 0), a, b)
-
             scene.addPath(transform.map(path), QPen(color))
 
-            for i in range(361):
-                angle = pi * i / 180 + initial
-                x, y = a * cos(angle), b * sin(angle)
-                point = QPointF(x * cos(alpha) - y * sin(alpha), x * sin(alpha) + y * cos(alpha))
-                l.append((i / 360, center + point))
+            initial = (qrand() % 360) * pi / 180
+            l = []
+            for i in range(p):
+                angle = 2 * pi * i / p + initial
+                point = transform.map(QPointF(a * cos(angle), b * sin(angle)))
+                l.append((i / (p - 1), point))
+
+            item = CircleObject(color=color)
+            animation = QPropertyAnimation(item, b'pos')
             animation.setKeyValues(l)
-            animation.setDuration(5000)
+            animation.setDuration(2000)
             group.addAnimation(animation)
             scene.addItem(item)
+
         group.start()
         group.finished.connect(group.start)
         self.graphicsView.setScene(scene)
