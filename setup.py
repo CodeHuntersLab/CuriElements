1a5ce489aab39fcb2c20c4c07a87e9158d341a61
eyllanesc <e.yllanescucho@gmail.com>
2016-11-14 20:19:23 -0500

    create setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='CuriElements',
+    version='0.1',
+    packages=['app'],
+    url='https://github.com/CodeHuntersLab/CuriElements',
+    license='LGPL3 GPL3',
+    author='eyllanesc',
+    author_email='e.yllanescuho@gmail.com',
+    description='Proyecto orientado a la enseñanza y aprendizaje de la tabla períodica '
+)
