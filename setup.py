709bfbed6da0ca7f9d325dda4073a421b1040a4f
eyllanesc <e.yllanescucho@gmail.com>
2016-11-14 22:12:02 -0500

    create setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='CuriElements',
-    version='0.1.4',
+    version='0.1.5',
     packages=['CuriElements'],
     url='https://github.com/CodeHuntersLab/CuriElements',
     license='LGPL3 GPL3',
